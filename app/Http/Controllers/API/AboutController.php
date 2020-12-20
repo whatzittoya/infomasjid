fa3760f6458e7f57bd884c9dfe0a1a61d91b11b6
whatzittoya <whosendall@gmail.com>
2020-12-20 14:38:43 +0700

    add about

--- app/Http/Controllers/API/AboutController.php
+++ app/Http/Controllers/API/AboutController.php
@@ -9,6 +9,18 @@ class AboutController extends Controller
 {
     public function index()
     {
-        return response()->json(array("about"=>"<p>ABOUT</p>"), 200);
+        $about='<p>Aplikasi InfoMasjid dikembangkan dan dinaungi oleh:</p>
+
+        <p><strong>YAYASAN INFOMASJID SYIAR ISLAM (YAYASAN INFOMASJID)</strong></p>
+
+        <p>Alamat : Perumahan Pandau Permai, Jl Mahang Raya Blok C 25 No.6, RT.002, RW.006, Desa Pandau Jaya Kecamatan Siak Hulu, Kabupaten Kampar, Provinsi Riau</p>
+
+        <p>Telp : 0812-3431-7274</p>
+
+        <p>email: cs.infomasjid@gmail.com</p>
+
+        <p>Website: <a href="http://infomasjid.my.id/">infomasjid.my.id</a></p>
+        ';
+        return response()->json(array("about"=>$about), 200);
     }
 }
